060a981b21ad8be474abcc7112d853b35a5c2775
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2023-04-25 20:50:39 +0300

    functions & directives

---
+++ functions-directives/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing :{
+        128: '32rem'
+      }
+    },
+  },
+  plugins: [],
+}
+
